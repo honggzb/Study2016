c5b19cfdfc9d5141f30f93e88cd53eefed5f43bc
Joanna Qian <honggzb@users.noreply.github.com>
2016-03-23 16:23:17 -0400

    Update alwaysTalking.js

--- nodeStudy/sample/02nodeModules/alwaysTalking.js
+++ nodeStudy/sample/02nodeModules/alwaysTalking.js
@@ -8,12 +8,12 @@ var sayings =[
   "It is far better to be alone, than to be in bad company."
 ];
 
-var interval = setInterval(function () {
+var interval = setInterval( () =>  {
   var i = Math.floor(Math.random()*sayings.length);
   process.stdout.write(`${sayings} \n`);
 }, 1000);
 
-process.stdin.on('data',function (data) {
+process.stdin.on('data', (data) => {
   console.log(`STDIN data received -> ${data.toString().trim()}`);
   clearInterval(interval);
   process.exit();
